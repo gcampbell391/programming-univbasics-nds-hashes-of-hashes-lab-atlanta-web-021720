bbecfcf1038b9e3993f900fc89ec7973a971a7ef
Gene Campbell III <gcampbell391@gmail.com>
2020-02-09 21:13:27 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -32,6 +32,9 @@ def naming_system
         label: "Phylum",
           sub_category: {
             label: "Class"
+              sub_category: {
+                label: "Order"
+              }
      }
    }
  }
