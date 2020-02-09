d451db3e387323e35f595d040192dbb1bfc2568c
Gene Campbell III <gcampbell391@gmail.com>
2020-02-09 21:20:57 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -40,9 +40,7 @@ def naming_system
                       label: "Genus",
                       sub_category: {
                         label: "Species",
-                        sub_category:{
-                          
-                        }
+                        sub_category: nil
                       }
                     }
                 }
